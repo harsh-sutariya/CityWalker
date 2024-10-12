15f5bdb1201d788ac4cdc3bbf11f4071025e62b0
Xinhao Liu <gavinliu_hm@hotmail.com>
2024-10-12 00:38:56 +0000

    add testint support

--- data/citywalk_dataset.py
+++ data/citywalk_dataset.py
@@ -52,10 +52,13 @@ class CityWalkDataset(Dataset):
             for f in sorted(os.listdir(self.pose_dir))
             if f.endswith('.txt')
         ]
+        print(len(self.pose_path))
         if mode == 'train':
             self.pose_path = self.pose_path[:cfg.data.num_train]
         elif mode == 'val':
             self.pose_path = self.pose_path[cfg.data.num_train:cfg.data.num_train + cfg.data.num_val]
+        elif mode == 'test':
+            self.pose_path = self.pose_path[cfg.data.num_train + cfg.data.num_val + cfg.data.num_test:]
         else:
             raise ValueError(f"Invalid mode {mode}")
 
@@ -169,7 +172,7 @@ class CityWalkDataset(Dataset):
         }
 
         # For visualization during validation
-        if self.mode == 'val':
+        if self.mode in ['val', 'test']:
             vis_input_positions = self.transform_poses(input_poses, current_pose)
             transformed_original_input_positions = self.transform_poses(original_input_poses, current_pose)
             target_transformed = self.transform_target_pose(target_pose, current_pose)
@@ -202,10 +205,11 @@ class CityWalkDataset(Dataset):
     def input2target(self, input_poses, target_pose):
         input_positions = input_poses[:, :3]
         target_position = target_pose[:3]
-        transformed_input_positions = input_positions - target_position
-        rand_angle = np.random.uniform(-np.pi, np.pi)
-        rot_matrix = np.array([[np.cos(rand_angle), -np.sin(rand_angle)], [np.sin(rand_angle), np.cos(rand_angle)]])
-        transformed_input_positions = transformed_input_positions[:, [0, 2]] @ rot_matrix.T
+        transformed_input_positions = (input_positions - target_position)[:, [0, 2]]
+        if self.mode == 'train':
+            rand_angle = np.random.uniform(-np.pi, np.pi)
+            rot_matrix = np.array([[np.cos(rand_angle), -np.sin(rand_angle)], [np.sin(rand_angle), np.cos(rand_angle)]])
+            transformed_input_positions = transformed_input_positions @ rot_matrix.T
         return transformed_input_positions
     
     def select_target_pose(self, future_poses):
